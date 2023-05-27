9e6d9341caf21217493a3c52d1d6461c40085ff3
DucHoang-UIT <hoangpd@unicloud.com.vn>
2023-05-27 13:31:27 +0700

    event sink

--- android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.test_app
 
 import android.content.Intent
-import android.os.Bundle
-import android.os.PersistableBundle
+import android.os.Handler
+import android.os.Looper
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.example.test_app/method_channel_activity"
     private  val EVENT_CHANNEL = "com.example.test_app/event_channel"
+    private var eventSink: EventChannel.EventSink? = null
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
@@ -31,6 +33,7 @@ class MainActivity: FlutterActivity() {
                         result.error("INVALID_ACTIVITY","Invalid activity name",null)
                     }
                 }
+
                 else -> result.notImplemented()
             }
         }
@@ -38,18 +41,53 @@ class MainActivity: FlutterActivity() {
 
         // Setup EventChannel
         val eventChannel = EventChannel(flutterEngine.dartExecutor.binaryMessenger,EVENT_CHANNEL)
-        eventChannel.setStreamHandler(object:EventChannel.StreamHandler{
-            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-                events?.success("Event 1")
-                events?.success("Event 2")
-                events?.success("Event 3")
-            }
+        eventChannel.setStreamHandler(
+            object:EventChannel.StreamHandler{
+                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                    eventSink = events
+                    sendData()
+                }
 
-            override fun onCancel(arguments: Any?) {
-                // Clean up resources
+                override fun onCancel(arguments: Any?) {
+                    eventSink =null
+                }
+
+                private fun sendData() {
+                    val timer = Timer()
+                    timer.scheduleAtFixedRate(object : TimerTask() {
+                        override fun run() {
+                            val data = "Current time: ${System.currentTimeMillis()}"
+                            Handler(Looper.getMainLooper()).post{
+                                eventSink?.success(data)
+                            }
+                        }
+                    }, 0, 1000)
+                }
             }
-        })
+        )
+
+
+
+
     }
+
+//    private fun createEventSink(): EventChannel.EventSink? {
+//        return object:EventChannel.EventSink{
+//            override fun success(event: Any?) {
+//                // Check if the event sink is set
+//                eventSink?.success(event)
+//            }
+//
+//            override fun error(errorCode: String?, errorMessage: String?, errorDetails: Any?) {
+//                eventSink?.error(errorCode,errorMessage,errorDetails)
+//            }
+//
+//            override fun endOfStream() {
+//              eventSink?.endOfStream()
+//            }
+//
+//        }
+//    }
 
     private fun startKotlinActivity(activityClass: Class<*>?) {
         val intent = Intent(this,activityClass)
